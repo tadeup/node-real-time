0ef0b2c25a1bbe7dfc2dd25624109b518b073447
unknown <tadeup1@gmail.com>
2018-10-09 22:14:34 -0300

    removed comment

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server!');
-
-    // socket.emit('createMessage', {
-    //     from: 'Marcelinho@gmail.com',
-    //     text: 'mercalauuuuuuuuuuuuuuum'
-    // });
 });
 
 socket.on('disconnect', function() {
